464f023953e4bcdd07fb9aa6def936e33192e0d7
Pradel Léo <pradel.leo@gmail.com>
2017-11-05 14:18:39 +0100

    setup gatsby and typescript

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `js-accounts`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-next`,
+    `gatsby-plugin-react-helmet`
+  ],
+}
