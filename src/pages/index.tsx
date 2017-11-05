428aaaf28b84a77b718d58bc9f0e4fc0d00a5edd
Pradel Léo <pradel.leo@gmail.com>
2017-11-05 14:25:34 +0100

    prettier and tslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 
 const IndexPage = () => (
   <div>
@@ -6,6 +6,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
