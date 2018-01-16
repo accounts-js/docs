2bd077dad32f2966cbe7471d2262925476875cf0
pradel <pradel.leo@gmail.com>
2018-01-16 15:29:52 +0100

    add gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           'gatsby-remark-prismjs'
         ],
       },
