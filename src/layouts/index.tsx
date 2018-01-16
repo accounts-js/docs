526939029fa1425d47dbdffd59873858bd17251b
pradel <pradel.leo@gmail.com>
2018-01-16 15:46:23 +0100

    github markdown theme

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import '../styles/style.scss';
-import '../styles/prism-base16-ateliersulphurpool.light.css';
+import '../styles/prism-github.css';
 
 const StyledContainer = styled.div`
   margin-top: 20px !important;
